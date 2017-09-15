7e7e8de992f13483edb6fcdb6c1a5bdfed385598
ShawnHumphries <sahump1@gmail.com>
2017-09-15 12:16:52 -0400

    JavaScript file for Todo list, utilizing jQuery  First commit contains basic functionality only

---
+++ js/todos.js
@@ -0,0 +1,23 @@
+// Check off specific todos by clicking on them
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+// Click on X to delete Todo
+$("ul").on("click", "span", function(event){
+	event.stopPropagation();
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+});
+
+$("input[type='text']").keypress(function(event){
+	if (event.which === 13) {
+		// grad todo text from input
+		var todoText = $(this).val();
+		// set the input field to empty
+		$(this).val("");
+		// create new li and add to ul
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+	}
+});
